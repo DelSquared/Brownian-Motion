9b8ddb53fe647fb5978b6e9ec34f3344d6be9e7f
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-06-08 14:48:43 +0200

    Update ODESolver.py

--- ODESolver.py
+++ ODESolver.py
@@ -9,64 +9,66 @@ def ODE (x,t,noisemap): #example ODE
   N=noisemap
   return 1j*(1+k*N)*x
 
-def integrator1(f,xo,t,N): #Order 1 (Euler's method)
-  x=np.zeros_like(t, dtype=complex)
-  x[0]=xo
+def integrator1(f,xo,t,N): #Order 3
+  x=np.zeros(shape=(xo.shape[0],t.shape[0]), dtype=complex)
+  x[:,0]=xo
   dt=t[1]-t[0]
   for i in range(t.shape[0]-1):
-    x[i+1]=x[i]+f(x[i],t[i],N[i])*dt
+    x[:,i+1]=x[:,i]+f(x[:,i],t[i], N[i])*dt
   return x
 
-def integrator2(f,xo,t,N): #Order 2 (Modified Euler/Midpoint method)
-  x=np.zeros_like(t, dtype=complex)
-  x[0]=xo
+def integrator2(f,xo,t,N): #Order 3
+  x=np.zeros(shape=(xo.shape[0],t.shape[0]), dtype=complex)
+  x[:,0]=xo
   dt=t[1]-t[0]
   for i in range(t.shape[0]-1):
-    k1=f(x[i], t[i], N[i])
-    x[i+1]=x[i]+f(x[i]+k1*dt/2, t[i]+dt/2, N[i])*dt
+    k1=f(x[:,i],t[i], N[i])
+    x[:,i+1]=x[:,i]+f( x[:,i]+k1*dt/2 , t[i]+dt/2 ,N[i])*dt
   return x
 
 def integrator3(f,xo,t,N): #Order 3
-  x=np.zeros_like(t, dtype=complex)
-  x[0]=xo
+  x=np.zeros(shape=(xo.shape[0],t.shape[0]), dtype=complex)
+  x[:,0]=xo
   dt=t[1]-t[0]
   for i in range(t.shape[0]-1):
-    k1=f(x[i],t[i], N[i])
-    k2=f(x[i]+k1*dt/2, t[i]+dt/2, N[i])
-    x[i+1]=x[i]+f( x[i]+k2*dt/2 , t[i]+dt/2 ,N[i])*dt
+    k1=f(x[:,i],t[i], N[i])
+    k2=f(x[:,i]+k1*dt/2, t[i]+dt/2, N[i])
+    x[:,i+1]=x[:,i]+f( x[:,i]+k2*dt/2 , t[i]+dt/2 ,N[i])*dt
   return x
 
-def integrator4(f,xo,t,N): #Order 4 (still requires a more exhaustive testing to be carried out)
-  x=np.zeros_like(t, dtype=complex)
-  x[0]=xo
+def integrator4(f,xo,t,N): #Order 3
+  x=np.zeros(shape=(xo.shape[0],t.shape[0]), dtype=complex)
+  x[:,0]=xo
   dt=t[1]-t[0]
   for i in range(t.shape[0]-1):
-    k1=f(x[i],t[i],N[i])
-    k2=f(x[i]+k1*dt/2, t[i]+dt/2, N[i])
-    k3=f(x[i]+k2*dt/2, t[i]+dt/2, N[i])
-    x[i+1]=x[i]+f(x[i]+k3*dt/2, t[i]+dt/2, N[i])*dt
+    k1=f(x[:,i],t[i], N[i])
+    k2=f(x[:,i]+k1*dt/2, t[i]+dt/2, N[i])
+    k3=f(x[:,i]+k2*dt/2, t[i]+dt/2, N[i])
+    x[:,i+1]=x[:,i]+f( x[:,i]+k3*dt/2 , t[i]+dt/2 ,N[i])*dt
   return x
 
-def integrator5(f,xo,t,N): #Order 5 (still requires a more exhaustive testing to be carried out)
-  x=np.zeros_like(t, dtype=complex)
-  x[0]=xo
+def integrator4(f,xo,t,N): #Order 3
+  x=np.zeros(shape=(xo.shape[0],t.shape[0]), dtype=complex)
+  x[:,0]=xo
   dt=t[1]-t[0]
   for i in range(t.shape[0]-1):
-    k1=f(x[i],t[i],N[i])
-    k2=f(x[i]+k1*dt/2, t[i]+dt/2, N[i])
-    k3=f(x[i]+k2*dt/2, t[i]+dt/2, N[i])
-    k4=f(x[i]+k3*dt/2, t[i]+dt/2, N[i])
-    x[i+1]=x[i]+f(x[i]+k4*dt/2, t[i]+dt/2, N[i])*dt
+    k1=f(x[:,i],t[i], N[i])
+    k2=f(x[:,i]+k1*dt/2, t[i]+dt/2, N[i])
+    k3=f(x[:,i]+k2*dt/2, t[i]+dt/2, N[i])
+    k4=f(x[:,i]+k3*dt/2, t[i]+dt/2, N[i])
+    x[:,i+1]=x[:,i]+f( x[:,i]+k4*dt/2 , t[i]+dt/2 ,N[i])*dt
   return x
 
 t=np.linspace(0,50,5000)
 N=np.random.normal(0,1,size=(t.shape[0]))
 
-x1=integrator1(ODE,5+1j,t,N) #Trying all solvers for comparison
-x2=integrator2(ODE,5+1j,t,N)
-x3=integrator3(ODE,5+1j,t,N)
-x4=integrator4(ODE,5+1j,t,N)
-x5=integrator5(ODE,5+1j,t,N)
+xo=np.array([5+1j])
+
+x1=integrator1(ODE,xo,t,N) #Trying all solvers for comparison
+x2=integrator2(ODE,xo,t,N)
+x3=integrator3(ODE,xo,t,N)
+x4=integrator4(ODE,xo,t,N)
+x5=integrator5(ODE,xo,t,N)
 
 fig=plt.figure(figsize=((2**13)/100,(2**10)/100)) #Plotting
 plt.plot(t,np.real(x1),label="real amplitude(1)")
